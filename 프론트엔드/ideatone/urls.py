2ab5ef73ddeb60a38bdc5329ed4d6d7e652b60e5
AlRanGaMola <saraoe11@naver.com>
2019-07-23 02:07:40 +0900

    test

---
+++ 프론트엔드/ideatone/urls.py
@@ -0,0 +1,40 @@
+"""ideatone URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+import inidea.views#블로그안에 있는 views라는 파이썬 파일을 가져옴
+
+from django.conf import settings#내 세팅을 가져와라
+from django.conf.urls.static import static#url들을 가져와라
+
+urlpatterns = [
+    path('love/',inidea.views.love, name="love"),
+    path('read/', inidea.views.read, name = "read"),
+    path('read1/', inidea.views.read1, name= "read1"),
+    path('admin/', admin.site.urls),
+    path('', inidea.views.main, name="main"),
+    path('admin/', admin.site.urls),
+    path('bulletin/', inidea.views.bulletin, name="bulletin"),
+    path('bulletin/intro/', inidea.views.bulletinIntro, name="bulletinIntro"),
+    path('bulletin/menu/', inidea.views.bulletinMenu, name="bulletinMenu"),
+    path('bulletin/map/', inidea.views.bulletinMap, name="bulletinMap"),
+    path('bulletin/com/', inidea.views.bulletinComment, name="bulletinComment"),
+    path('new/',inidea.views.new, name="new"),
+    path('pr2/',inidea.views.pr2, name="pr2"),
+    path('pr22/',inidea.views.pr2, name="pr22"),
+    path('pratice/',inidea.views.pratice, name="pratice"),
+]
