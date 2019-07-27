2e93c825454feb4e99679225334df10d63a54456
AlRanGaMola <saraoe11@naver.com>
2019-07-27 23:59:40 +0900

    'test'

--- 백엔드/instargram/photo/models.py
+++ 백엔드/instargram/photo/models.py
@@ -9,9 +9,15 @@ class Photo(models.Model):
     image = models.ImageField(upload_to= 'timeline_photo/%Y/%m/%d')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    approved_comment = models.BooleanField(default=False)
 
     like = models.ManyToManyField(User, related_name = 'like_post', blank=True)
     favorite = models.ManyToManyField(User, related_name='favorite_post', blank=True)
+
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
 
     def __str__(self):
         return "text : "+self.text
@@ -22,6 +28,20 @@ class Photo(models.Model):
     def get_absolute_url(self):
         return reverse('photo:detail', args=[self.id])
 
+class Comment(models.Model):
+    post = models.ForeignKey(User, on_delete=models,related_name = 'comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+        
+    def __str__(self):
+        return "text : "+self.text
+        
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField("data published")
